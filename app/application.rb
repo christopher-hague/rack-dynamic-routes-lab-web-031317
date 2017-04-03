43fd03f5a61015c9d60338557f0ae4397faecfec
christopher-hague <christopher.a.hague@gmail.com>
2017-04-03 17:10:50 -0400

    submit 2

--- app/application.rb
+++ app/application.rb
@@ -9,8 +9,9 @@ class Application
     if req.path.match("/items")
       # item name
       item_name = req.path.split("/items/").last # turns /items/#item into #item
+      item = @@items.find { |i| i.name == item_name }
 
-      if item =@@items.find { |i| i.name == item_name }
+      if @@items.include?(item)
         resp.write item.price
       else
         resp.write "Item not found"
